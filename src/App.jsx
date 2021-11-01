413430f5f92b191ce999ace983ce0eec41ed78c1
Matilda Mared <matildamared@TildasMacBookPro.local>
2021-11-01 08:59:08 +0100

    add global basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import { useState } from "react";
+import styled from "styled-components";
+import Button from "./components/Button";
 
 function App() {
 	return (
-		<div className="App">
-			<h1>Memory</h1>
-		</div>
+		<Wrapper>
+			<Heading>An amazing memory game.</Heading>
+			<Button>Start Game</Button>
+		</Wrapper>
 	);
 }
+
+const Wrapper = styled.div`
+	max-width: 860px;
+	margin: 40px auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Heading = styled.h1`
+	font-size: 60px;
+	font-weight: 800;
+	letter-spacing: -1px;
+`;
 
 export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+function Button(props) {
+	return <Wrapper>{props.children}</Wrapper>;
+}
+
+const Wrapper = styled.button`
+  background: none;
+  border: 2px solid var(--color-light);
+  font: inherit;
+  color: inherit;
+  font-weight: 700;
+  cursor: pointer;
+  font-size: 1rem;
+  padding: 8px 16px;
+  border-radius: 8px;
+
+  &:hover {
+    background-color: mediumpurple;
+  }
+`
+
+export default Button;
